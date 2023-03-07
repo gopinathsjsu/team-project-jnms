2eed55ca25e6cbe80c7d6c102bfc8ac5fd5a0b01
Spartan <spartan@IMS-130MBA.local>
2023-03-07 12:25:30 -0800

    added controller and about Gym activities controller

---
+++ backend/api/controllers/aboutGymController.js
@@ -0,0 +1,23 @@
+const aboutGym = require('../models/aboutGym');
+
+/**
+ * 
+ * @param {Object} request 
+ * @param {Object} response
+ * 
+ * Fetch About Gym 
+ */
+exports.aboutGymInformation = (request, response) => {
+    aboutGym.find({"info.cities.city": request.params.city}, {"info.memberships": 1, "info.cities.$": 1})
+    .exec()
+    .then(documents => {
+        console.log("Success fetching about gym in aboutGymInformation", documents);
+        
+        response.status(200).json(documents[0]);
+    })
+    .catch(error => {
+        console.log("Error fetching about gym in aboutGymInformation", error);
+        
+        response.status(500).json(error);
+    });
+};
