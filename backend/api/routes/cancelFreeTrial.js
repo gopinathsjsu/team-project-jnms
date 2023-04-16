60e73e154d07ab3e0d14475d24832bea2e5aa68d
Shreya <kshreya.reddy@gmail.com>
2023-04-15 19:27:35 -0700

    added route

---
+++ backend/api/routes/cancelFreeTrial.js
@@ -0,0 +1,35 @@
+// const express = require('express');
+
+// const cancelFreeTrialController = require('../controllers/cancelFreeTrialController');
+
+// const router = express.Router();
+
+// router.get('/', (request, response, next) => {
+//     console.log("Route to cancelFreeTrial", request.body);
+    
+//     cancelFreeTrialController.getEmailId(request, response);
+// });
+
+// router.post('/', (request, response, next) => {
+//     console.log("Route to cancelFreeTrial", request.body);
+    
+//     cancelFreeTrialController.cancelFreeTrial(request, response);
+// });
+
+// module.exports = router;
+
+const express = require('express');
+const cancelFreeTrialController = require('../controllers/cancelFreeTrialController');
+const router = express.Router();
+
+router.get('/', (request, response, next) => {
+  console.log("Route to cancelFreeTrial", request.body);
+  cancelFreeTrialController.getEmailId(request, response);
+});
+
+router.post('/', (request, response, next) => {
+  console.log("Route to cancelFreeTrial", request.body);
+  cancelFreeTrialController.cancelFreeTrial(request, response);
+});
+
+module.exports = router;
